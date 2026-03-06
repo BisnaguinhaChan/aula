0975cb300a21d6150c800a56a8713859b5c20e06
JulietaBisnaguinha <bisnaguinha201019@gmail.com>
2026-03-06 15:41:05 -0300

    alteração do numero 10 para 9 na linha subritação do consolelog

--- aula_classes.js
+++ aula_classes.js
@@ -21,7 +21,7 @@ function dividir(a, b) {
 
 // Testando as funções
 console.log("Soma: 5 + 3 =", somar(5, 3));
-console.log("Subtração: 10 - 4 =", subtrair(10, 4));
+console.log("Subtração: 9 - 4 =", subtrair(9, 4));
 console.log("Multiplicação: 6 x 7 =", multiplicar(6, 7));
 console.log("Divisão: 20 / 4 =", dividir(20, 4));
 console.log("Divisão por zero:", dividir(10, 0));
